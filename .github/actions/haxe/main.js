cc4724455bf5deadc01d992381c058418d7e6b19
Ian Harrigan <ianharrigan@hotmail.com>
2019-12-12 15:05:20 +0100

    test

--- .github/actions/haxe/main.js
+++ .github/actions/haxe/main.js
@@ -4,6 +4,7 @@ const http = require('http');
 const fs = require('fs');
 const child_process = require('child_process');
 
+/*
 var download = function(url, dest, cb) {
     var file = fs.createWriteStream(dest);
     var request = http.get(url, function(response) {
@@ -16,6 +17,7 @@ var download = function(url, dest, cb) {
       if (cb) cb(err.message);
     });
   };
+*/
 
 try {
     var haxeVersion = core.getInput('haxe-version');
@@ -25,9 +27,10 @@ try {
     var archiveUrl = "http://github.com/HaxeFoundation/haxe/releases/download/4.0.3/" + filename;
     console.log("Downloading haxe from: " + archiveUrl);
 
-    download(archiveUrl, filename, function() {
-        console.log("Download complete");
-        console.log("Expanding " + filename);
+//    download(archiveUrl, filename, function() {
+//        console.log("Download complete");
+//        console.log("Expanding " + filename);
+        child_process.execSync('wget https://github.com/HaxeFoundation/haxe/releases/download/4.0.3/haxe-4.0.3-linux64.tar.gz', {stdio: 'inherit'});
         fs.mkdirSync("/opt/haxe");
         child_process.execSync('tar -C /opt/haxe -zxvf ' + filename + ' --strip 1', {stdio: 'inherit'});
         //child_process.execSync("tar", ["-C", "/opt/haxe", "-zxvf", filename, "--strip", "1"]);
@@ -35,7 +38,7 @@ try {
         child_process.execSync("chmod 777 /opt/haxe/haxelib");
         fs.mkdirSync("/opt/haxelib");
         child_process.execSync("/opt/haxe/haxelib setup /opt/haxelib");
-    })
+//    })
 
     /*
     const file = fs.createWriteStream(filename);

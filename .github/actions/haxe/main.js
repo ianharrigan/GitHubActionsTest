a626d8da1d466b10c5fac4b4c95b76cf1b826c17
Ian Harrigan <ianharrigan@hotmail.com>
2019-12-12 14:42:00 +0100

    test

--- .github/actions/haxe/main.js
+++ .github/actions/haxe/main.js
@@ -1,9 +1,21 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const http = require('http');
+const fs = require('fs');
 
 try {
-    const haxeVersion = core.getInput('haxe-version');
-    console.log("-------------------------> RUNNING ACTION: " + haxeVersion);
+    var haxeVersion = core.getInput('haxe-version');
+    var platform = core.getInput('platform');
+
+    var filename = "haxe-4.0.3-linux64.tar.gz";
+    var archiveUrl = "https://github.com/HaxeFoundation/haxe/releases/download/4.0.3/" + filename;
+    console.log("downloading haxe from: " + archiveUrl);
+
+    const file = fs.createWriteStream(filename);
+    const request = http.get(archiveUrl, function(response) {
+        console.log("Download complete");
+        response.pipe(file);
+    });
 } catch (error) {
     core.setFailed(error.message);
 }

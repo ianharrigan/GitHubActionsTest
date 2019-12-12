415c2acf152bf386be8f3b644ea49563d90c901a
Ian Harrigan <ianharrigan@hotmail.com>
2019-12-12 14:56:59 +0100

    test

--- .github/actions/haxe/main.js
+++ .github/actions/haxe/main.js
@@ -29,7 +29,8 @@ try {
         console.log("Download complete");
         console.log("Expanding " + filename);
         fs.mkdirSync("/opt/haxe");
-        child_process.execSync("tar -C /opt/haxe -zxvf " + filename + " --strip 1");
+        //child_process.execSync("tar -C /opt/haxe -zxvf " + filename + " --strip 1");
+        child_process.execSync("tar" ["-C", "/opt/haxe", "-zxvf", filename, "--strip", "1"]);
         child_process.execSync("chmod 777 /opt/haxe/haxe");
         child_process.execSync("chmod 777 /opt/haxe/haxelib");
         fs.mkdirSync("/opt/haxelib");
